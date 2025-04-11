da0ef2e71fcd1cef62449d06741aa52f23e0a3cf
Koichi Shiraishi <zchee.io@gmail.com>
2025-04-11 12:51:23 +0900

    a2a: add initial stub
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ a2a.go
@@ -0,0 +1,5 @@
+// Copyright 2025 The Go A2A Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Package a2a provides an open protocol enabling communication and interoperability between opaque agentic applications for Go.
+package a2a
