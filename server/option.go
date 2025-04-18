e71c5a38db5eed60111ee79d05f9991ce96784d3
Koichi Shiraishi <zchee.io@gmail.com>
2025-04-18 09:24:11 +0900

    server: change method to function to ServerOption
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- server/option.go
+++ server/option.go
@@ -13,21 +13,21 @@ import (
 type ServerOption func(*Server)
 
 // WithEndpoint sets the custom endpoint for the [Server].
-func (s *Server) WithEndpoint(endpoint string) ServerOption {
+func WithEndpoint(endpoint string) ServerOption {
 	return func(s *Server) {
 		s.endpoint = endpoint
 	}
 }
 
 // WithLogger sets the [*slog.Logger] for the [Server].
-func (s *Server) WithLogger(logger *slog.Logger) ServerOption {
+func WithLogger(logger *slog.Logger) ServerOption {
 	return func(s *Server) {
 		s.logger = logger
 	}
 }
 
 // WithTracer sets the [trace.Tracer] for the [Server].
-func (s *Server) WithTracer(tracer trace.Tracer) ServerOption {
+func WithTracer(tracer trace.Tracer) ServerOption {
 	return func(s *Server) {
 		s.tracer = tracer
 	}
