4b1eef0bd697cb7146769e1942dd50425649fe94
Koichi Shiraishi <zchee.io@gmail.com>
2025-04-22 02:26:31 +0900

    a2a: move constants to payload.go
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- payload.go
+++ payload.go
@@ -9,6 +9,24 @@ import (
 	"github.com/bytedance/sonic"
 )
 
+// A2A RPC method names.
+const (
+	// MethodTasksSend is the method name for sending a task.
+	MethodTasksSend = "tasks/send"
+	// MethodTasksGet is the method name for getting a task.
+	MethodTasksGet = "tasks/get"
+	// MethodTasksCancel is the method name for canceling a task.
+	MethodTasksCancel = "tasks/cancel"
+	// MethodTasksPushNotificationSet is the method name for setting push notification configuration.
+	MethodTasksPushNotificationSet = "tasks/pushNotification/set"
+	// MethodTasksPushNotificationGet is the method name for getting push notification configuration.
+	MethodTasksPushNotificationGet = "tasks/pushNotification/get"
+	// MethodTasksSendSubscribe is the method name for sending a task and subscribing to updates.
+	MethodTasksSendSubscribe = "tasks/sendSubscribe"
+	// MethodTasksResubscribe is the method name for resubscribing to task updates.
+	MethodTasksResubscribe = "tasks/resubscribe"
+)
+
 // SendTaskRequest represents a request to initiate or continue a task.
 type SendTaskRequest struct {
 	JSONRPCMessage
